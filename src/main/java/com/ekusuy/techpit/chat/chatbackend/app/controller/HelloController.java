ec123e67f4d61f72a23056e096efe956f2e375c7
ekusuy <ekusuy.omi@gmail.com>
2022-11-15 00:25:27 +0900

    first commit

---
+++ src/main/java/com/ekusuy/techpit/chat/chatbackend/app/controller/HelloController.java
@@ -0,0 +1,19 @@
+package com.ekusuy.techpit.chat.chatbackend.app.controller;
+
+import java.util.Optional;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.ekusuy.techpit.chat.chatbackend.domain.hello.model.Hello;
+
+@RestController
+public class HelloController {
+
+  @GetMapping("/hello")
+  public Hello hello(@RequestParam("name") Optional<String> name) {
+    String resName = name.orElse("world!");
+    return new Hello("Hello, " + resName);
+  }
+}
---
+++ src/main/java/com/ekusuy/techpit/chat/chatbackend/domain/channels/model/Channel.java
@@ -0,0 +1,9 @@
+package com.ekusuy.techpit.chat.chatbackend.domain.channels.model;
+
+import lombok.Data;
+
+@Data
+public class Channel {
+    private int id;
+    private String name;
+}
