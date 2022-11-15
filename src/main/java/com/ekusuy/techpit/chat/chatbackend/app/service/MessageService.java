573e613fd4bc1809408c97255304ba323677cead
ekusuy <ekusuy.omi@gmail.com>
2022-11-15 22:43:11 +0900

    メッセージのサービス層追加

---
+++ src/main/java/com/ekusuy/techpit/chat/chatbackend/app/service/MessageService.java
@@ -0,0 +1,22 @@
+package com.ekusuy.techpit.chat.chatbackend.app.service;
+
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.ekusuy.techpit.chat.chatbackend.domain.messages.model.Message;
+import com.ekusuy.techpit.chat.chatbackend.domain.messages.service.MessageDomainService;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@Transactional
+@RequiredArgsConstructor
+public class MessageService {
+    private final MessageDomainService messageDomainService;
+
+    public Message post(Message message){
+        var username = "guest";
+        message.setUsername(username);
+        return messageDomainService.post(message);
+    }
+}
